3b7c092a9e9dd1e2d2325cd7594c83b00e66544b
Chris <cmjoy136@gmail.com>
2021-06-22 15:15:51 -0400

    set media query styles, renamed ids, fixed jquery scroll affects from body and html css properties

--- js/main.js
+++ js/main.js
@@ -1,61 +1,71 @@
- $(function(){
-    //Nav bar scrolling
-    $('.page-sections li a').on('click', function(e) {
-        let href = $(this).attr('href')
-        $('.current-section').removeClass('current-section')
+$(function () {
+  //Nav bar scrolling
+  $(".page-sections li a").on("click", function (e) {
+    e.preventDefault();
+    let href = $(this).attr("href");
+    console.log(href);
+    $(".current-section").removeClass("current-section");
+    $("html, body").animate(
+      {
+        scrollTop: $(`${href}`).offset().top,
+      },
+      200
+    );
+  });
 
-        $('html, body').animate({
-            scrollTop: $(href).offset().top
-        }, 200)
-        
-        e.preventDefault()
-      })
+  //remove collapse on resize
+  $(window).on("resize load", function () {
+    let win = $(this);
+    if (win.width() < 1200) {
+      $(".sidebar").addClass("collapsed");
+      $(".navbar").removeClass("collapsed");
+    } else {
+      $(".navbar").addClass("collapsed");
+      $(".sidebar").removeClass("collapsed");
+    }
+  });
 
-      //remove collapse on resize
-      $(window).on('resize', function() {
-        let win=$(this)
-        if(win.width() < 1000 ){
+  //reveal sidebar on click
+  $(".nav-button").on("click", function () {
+    let sidebar = $(".sidebar");
+    sidebar.toggleClass("collapsed");
+    setTimeout(() => {
+      sidebar.toggleClass("collapsed");
+    }, 10000);
+  });
 
-          $('.sidebar').addClass('collapsed')
-          $('.navbar').removeClass('collapsed')
-        } else {
-          $('.navbar').addClass('collapsed')
-          $('.sidebar').removeClass('collapsed')
+  //active navbar on scroll
+  $(document).on("scroll", function () {
+    let scroll = $(document).scrollTop();
+    let aboutOffset = $("#about").offset().top - 200;
+    let projectOffset = $("#projects").offset().top - 150;
+    let expOffset = $("#experience").offset().top - 300;
+    let conOffset = $('#contact').offset().top
 
-        }
-      })
+    console.log(conOffset, expOffset, scroll)
 
-      //active navbar on scroll
-      $(window).scroll(function(){
-        let scroll = $(document).scrollTop()
-        let aboutOffset = $('#about').offset().top-3
-        let projectOffset = $('#projects').offset().top-10
-        let expOffset = $('#experience').offset().top-300
+    if (scroll >= aboutOffset && scroll < projectOffset) {
+      $("#about-tab").addClass("current-section");
+    } else {
+      $("#about-tab").removeClass("current-section");
+    }
+    if (scroll >= projectOffset && scroll < expOffset) {
+      $("#proj-tab").addClass("current-section");
+      $("#about-tab").removeClass("current-section");
+    } else {
+      $("#proj-tab").removeClass("current-section");
+    }
+    if (scroll >= expOffset && scroll < expOffset + 200) {
+      $("#exp-tab").addClass("current-section");
+      $("#proj-tab").removeClass("current-section");
+    } else {
+      $("#exp-tab").removeClass("current-section");
+    }
+  });
 
-        if(scroll >= aboutOffset) {
-          $('#about-tab').addClass('current-section')
-        } else {
-          $('#about-tab').removeClass('current-section')
-        }
-        
-        if(scroll >= projectOffset) {
-          $('#project-tab').addClass('current-section')
-          $('#about-tab').removeClass('current-section')
-        } else {
-          $('#project-tab').removeClass('current-section')
-        }
-        if(scroll >= expOffset) {
-          $('#experience-tab').addClass('current-section')
-          $('#project-tab').removeClass('current-section')
-        } else {
-          $('#experience-tab').removeClass('current-section')
-        }
-      })
-
-      $('#contact-tab').on('click', function(){
-        $('.current-section').removeClass('current-section')
-        $(this).addClass('current-section')
-      })
-
-    });
-
+  //set button as active tab when clicked on vs scroll
+  $(".page-section").on("click", function () {
+    $(".current-section").removeClass("current-section");
+    $(this).addClass("current-section");
+  });
+});
